8cecb806cd6c75008c65e921787bc3ad5f493543
´´Hugo240306´´ <hugosuarezbarreiro06@liceolapaz.net>
2025-12-08 00:27:49 +0100

    Bucles de colocar los barcos

--- src/Barcos.java
+++ src/Barcos.java
@@ -1,17 +1,48 @@
+import java.util.Random;
+
 public class Barcos {
 
     public static void colocarBarcosAleatorios(int[][] tableroBarcos, int[] tamanosBarcos) {
-    }
+
+        Random rnd = new Random();
+        for (int id = 0; id < tamanosBarcos.length; id++) {
+            int tam = tamanosBarcos[id];
+
+            boolean colocado = false;
+
+            int fila = rnd.nextInt(tableroBarcos.length);
+            int col = rnd.nextInt(tableroBarcos[0].length);
+            boolean horizontal = rnd.nextBoolean();
+
+            if (sePuedeColocarBarco(tableroBarcos, fila, col, tam, horizontal)) {
+                colocarBarco(tableroBarcos, fila, col, tam, horizontal, id);
+                colocado = true;
+            }
+
+               }
+        }
 
     public static boolean sePuedeColocarBarco(int[][] tablero, int fila, int columna, int tamano, boolean horizontal) {
-        return false;
+        if (horizontal) {
+            return columna + tamano <= tablero[0].length;
+        } else {
+            return fila + tamano <= tablero.length;
+            }
     }
 
     public static void colocarBarco(int[][] tablero, int fila, int columna, int tamano, boolean horizontal, int idBarco) {
+        if (horizontal) {
+            for (int c = columna; c < columna + tamano; c++) {
+                tablero[fila][c] = idBarco;
+            }
+        } else {
+            for (int f = fila; f < fila + tamano; f++) {
+                tablero[f][columna] = idBarco;
+            }
+                }
     }
 
     public static boolean todosHundidos(int[] impactos, int[] tamanosBarcos) {
         return false;
-    }
+        }
 }
-
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static final int FILAS = 10;
     public static final int COLUMNAS = 10;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
         int[] tamanosBarcos = {5, 4, 3, 3, 2};
         int numBarcos = tamanosBarcos.length;
@@ -19,12 +16,12 @@ public class Main {
 
         int[] impactosJugador = new int[numBarcos];
         int[] impactosCPU = new int[numBarcos];
-
         for (int i = 0; i < numBarcos; i++) {
             impactosJugador[i] = 0;
             impactosCPU[i] = 0;
         }
-        
-        sc.close();
-    }
+
+        Barcos.colocarBarcosAleatorios(tableroBarcosJugador, tamanosBarcos);
+        Barcos.colocarBarcosAleatorios(tableroBarcosCPU, tamanosBarcos);
+        }
 }
